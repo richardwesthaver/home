ee7ffa55c47e3259ddfd57d864748bc6c3e5b934
Richard Westhaver <ellis@rwest.io>
2024-06-04 21:46:48 -0400

    sk templates

--- .emacs.d/ellis.el
+++ .emacs.d/ellis.el
@@ -97,7 +97,7 @@
    smtpmail-debug-info t
    message-default-mail-headers "Cc: \nBcc: \n"
    message-kill-buffer-on-exit t
-   user-mail-address "ellis@rwest.io"
+   user-mail-address "richard.westhaver@gmail.com"
    user-full-name "Richard Westhaver"
    notmuch-hello-sections '(notmuch-hello-insert-saved-searches 
                             notmuch-hello-insert-search 
@@ -213,20 +213,6 @@
 (use-package sh-script
   :hook (sh-mode . flymake-mode))
 
-(use-package tempo
-  :custom
-  tempo-interactive t
-  :config
-  (tempo-define-template 
-   "org:readme"
-   '("#+TITLE: " p n>
-     "#+AUTHOR: " user-full-name " <" user-mail-address ">" n>)
-   "org:readme"
-   "Insert a readme.org file template.")
-  (tempo-define-template "org:src"
-                         '("#+begin_src " p n>
-                           "#+end_src" n>)
-                         "org:src"))
 ;;; Org Config
 (keymap-set user-map "t" #'org-todo)
 
@@ -281,7 +267,7 @@
   (add-hook 'ibuffer-mode-hook #'all-the-icons-ibuffer-mode))
 
 ;; strangerdanger
-(setq slime-enable-evaluate-in-emacs t)
+;; (setq slime-enable-evaluate-in-emacs t)
 
 (defun org-word-count (beg end
                            &optional count-latex-macro-args?
@@ -566,97 +552,123 @@ EXT is a list of the extensions of files to be included."
 (add-hook 'after-init-hook 'org-set-agenda-files)
 
 ;;; Skel Config
+(use-package skel
+  :requires skel
+  :load-path user-emacs-lib-directory
+  :custom
+  tempo-interactive t
+  auto-insert 'unmodified
+  auto-insert-query nil
+  skt-enable-tempo-elements t
+  skt-delete-duplicate-marks t
+  :config
+  (defvar skt-default-version "0.1.0")
+  (skt-define-template readme (:mode org-mode :tag t)
+    "#+title: " (p "title: ") n
+    "#+description: " (p "description: ") n
+    "#+author: " user-full-name n
+    "#+email:" user-mail-address n
+    "#+setupfile: clean.theme" n
+    "#+export_file_name: index" n>
+    p n> n>
+    ":info:" n>
+    "+ version :: " skt-default-version n
+    ":end:" n>)
+  (keymap-set skt-minor-mode-map "b" #'tempo-backward-mark)
+  (keymap-set skt-minor-mode-map "f" #'tempo-forward-mark)
+  (keymap-set skt-minor-mode-map "SPC" #'tempo-complete-tag)
+  (keymap-set skt-minor-mode-map "t" #'skt-add-tag)
 
-(setopt skt-enable-tempo-elements t
-        skt-delete-duplicate-marks t)
-
-(keymap-set skt-minor-mode-map "b" #'tempo-backward-mark)
-(keymap-set skt-minor-mode-map "f" #'tempo-forward-mark)
-(keymap-set skt-minor-mode-map "SPC" #'tempo-complete-tag)
-(keymap-set skt-minor-mode-map "t" #'skt-add-tag)
-
-(skt-define-template defmacro (:abbrev "defvar" :tag t :mode lisp-mode)
-  "(defmacro " (p "Name: ") " (" (p "Args: ") ")" > n> r ")")
-
-(skt-define-template defun (:abbrev "defvar" :tag t :mode lisp-mode)
-  "(defun " (p "Name: ") " (" (p "Args: ") ")" > n> r ")")
-
-(skt-define-template defvar (:abbrev "defvar" :tag t :mode lisp-mode)
-  > "(defvar " > r ")")
-
-(skt-define-function capture (:abbrev "capture" :tag t) org-capture)
-(skt-define-function agenda (:abbrev "agenda" :tag t) org-agenda)
-(skt-define-function mjump (:abbrev "mjump" :tag t) bookmark-jump)
-(skt-define-function bjump (:abbrev "bjump" :tag t) ibuffer-jump)
-(skt-define-function rjump (:abbrev "rjump" :tag t)
-  (lambda () (jump-to-register (read-char "register: "))))
-(skt-define-function pjump (:abbrev "pjump" :tag t) (lambda () (project-switch-project default-directory)))
-
-(defvar skt-skeleton-path-function #'abbreviate-file-name
-  "Function to be called when expanding file-header skeletons. Useful to
+  (defvar skt-skeleton-path-function #'abbreviate-file-name
+    "Function to be called when expanding file-header skeletons. Useful to
 rebind locally inside a project or module, where you want to delete some
 prefix or replace it.")
 
-(defun skt-buffer-path () (funcall skt-skeleton-path-function buffer-file-name))
+  (defun skt-buffer-path () (funcall skt-skeleton-path-function buffer-file-name))
 
-(defun skt-skelfile-path ()
-  (if (string= (file-name-nondirectory buffer-file-name) "skelfile")
-      "skelfile"
-    (skt-buffer-path)))
+  (defun skt-skelfile-path ()
+    (if (string= (file-name-nondirectory buffer-file-name) "skelfile")
+        "skelfile"
+      (skt-buffer-path)))
+  ;; TODO 2024-06-04: 
+  ;; (skt-define-template defsystem (:mode lisp-mode :tag t :abbrev "defsystem"))
+  ;; (skt-define-template defpackage (:mode lisp-mode :tag t :abbrev "defpackage"))
+  ;; (skt-define-template defpkg (:mode lisp-mode :tag t :abbrev "defpkg"))
+  
+  (skt-define-template defmacro (:abbrev "defvar" :tag t :mode lisp-mode)
+    "(defmacro " (p "Name: ") " (" (p "Args: ") ")" > n> r ")")
 
-(skt-define-skeleton head (:abbrev "head" :mode lisp-mode)
-  "title: "
-  ";;; " (skt-buffer-path) " --- " str \n \n \n ";;; Code:" \n > _)
+  (skt-define-template defun (:abbrev "defvar" :tag t :mode lisp-mode)
+    "(defun " (p "Name: ") " (" (p "Args: ") ")" > n> r ")")
 
-(skt-define-skeleton head (:abbrev "head" :mode rust-mode)
-  "title: "
-  "//! " (skt-buffer-path) " --- " str \n \n "// " _ \n \n "//! Code:" \n > _)
+  (skt-define-template defvar (:abbrev "defvar" :tag t :mode lisp-mode)
+    > "(defvar " > r ")")
 
-(skt-define-skeleton head (:abbrev "head" :mode skel-mode)
-  "title: "
-  ";;; " (skt-skelfile-path) " --- " str " -*- mode: skel; -*-" \n _)
+  (skt-define-function capture (:abbrev "capture" :tag t) org-capture)
+  (skt-define-function agenda (:abbrev "agenda" :tag t) org-agenda)
+  (skt-define-function mjump (:abbrev "mjump" :tag t) bookmark-jump)
+  (skt-define-function bjump (:abbrev "bjump" :tag t) ibuffer-jump)
+  (skt-define-function rjump (:abbrev "rjump" :tag t)
+    (lambda () (jump-to-register (read-char "register: "))))
+  (skt-define-function pjump (:abbrev "pjump" :tag t) (lambda () (project-switch-project default-directory)))
 
-(skt-define-skeleton head (:abbrev "head" :mode org-mode)
-  "title: "
-  "#+title: " str \n
-  "#+author: " (skeleton-read "author: ") \n
-  "#+description: " (skeleton-read "description: ") \n
-  "#+setupfile: clean.theme" \n > _)
+  (skt-define-skeleton head (:abbrev "head" :mode lisp-mode)
+    "title: "
+    ";;; " (skt-buffer-path) " --- " str \n \n \n ";;; Code:" \n > _)
 
-(skt-define-skeleton local-vars
-    (:tag t :abbrev "local-vars"
-     :docstring "Insert a local variables section.  Use current comment syntax if any.")
- (completing-read "Mode: " obarray
-		   (lambda (symbol)
-		     (if (commandp symbol)
-			 (string-match "-mode$" (symbol-name symbol))))
-		   t)
- '(save-excursion
-    (if (re-search-forward page-delimiter nil t)
-	 (error "Not on last page")))
- comment-start "Local Variables:" comment-end \n
- comment-start "mode: " str
- & -5 | '(kill-line 0) & -1 | comment-end \n
- ( (completing-read (format "Variable, %s: " skeleton-subprompt)
-		     obarray
+  (skt-define-skeleton system-head (:abbrev "system-head" :mode lisp-mode)
+    "name: "
+    ";;; " (skt-buffer-path) " --- "
+    '(setq v1 (file-name-base (skt-buffer-path))) (capitalize v1)
+    " Sytem Definitions" \n
+    > "(defsystem :" v1 \n
+    > ":depends-on (:std :log)" \n
+    > ":components ((:file \"pkg\")" _ "))")
+
+  (skt-define-skeleton head (:abbrev "head" :mode skel-mode)
+    "title: "
+    ";;; " (skt-skelfile-path) " --- " str " -*- mode: skel; -*-" \n _)
+
+  (skt-define-skeleton head (:abbrev "head" :mode org-mode)
+    "title: "
+    "#+title: " str \n
+    "#+author: " (skeleton-read "author: ") \n
+    "#+description: " (skeleton-read "description: ") \n
+    "#+setupfile: clean.theme" \n > _)
+
+  (skt-define-skeleton local-vars
+      (:tag t :abbrev "local-vars"
+            :docstring "Insert a local variables section.  Use current comment syntax if any.")
+    (completing-read "Mode: " obarray
 		     (lambda (symbol)
-		       (or (eq symbol 'eval)
-			   (custom-variable-p symbol)))
+		       (if (commandp symbol)
+			   (string-match "-mode$" (symbol-name symbol))))
 		     t)
-   comment-start str ": "
-   (read-from-minibuffer "Expression: " nil read-expression-map nil
-			  'read-expression-history) | _
-                          comment-end \n)
- resume:
- comment-start "End:" comment-end \n)
+    '(save-excursion
+       (if (re-search-forward page-delimiter nil t)
+	   (error "Not on last page")))
+    comment-start "Local Variables:" comment-end \n
+    comment-start "mode: " str
+    & -5 | '(kill-line 0) & -1 | comment-end \n
+    ( (completing-read (format "Variable, %s: " skeleton-subprompt)
+		       obarray
+		       (lambda (symbol)
+		         (or (eq symbol 'eval)
+			     (custom-variable-p symbol)))
+		       t)
+      comment-start str ": "
+      (read-from-minibuffer "Expression: " nil read-expression-map nil
+			    'read-expression-history) | _
+      comment-end \n)
+    resume:
+    comment-start "End:" comment-end \n)
 
-;; autoinsert
-(skt-register-auto-insert "skelfile" #'skt-template-skel-head)
-(setq auto-insert :unmodified)
-(setq auto-insert-query nil)
-(auto-insert-mode t)
-
-(keymap-set skel-minor-mode-map "C-<return>" 'company-tempo)
+  ;; autoinsert
+  (skt-register-auto-insert "skelfile" #'skt-template-skel-head)
+  (skt-register-auto-insert "readme.org" #'skt-template-org-readme)
+  (skt-register-auto-insert ".*[.]asd" #'skt-template-lisp-system-head)
+  (auto-insert-mode t)
+  (keymap-set skel-minor-mode-map "C-<return>" 'company-tempo))
 
 (provide 'ellis)
 ;;; ellis.el ends here
