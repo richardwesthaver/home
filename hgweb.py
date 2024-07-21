136efe77a4ed76e9e7b7be66450c9ce4dde0a970
Richard Westhaver <ellis@rwest.io>
2024-07-20 22:31:54 -0400

    init vc

---
+++ hgweb.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+config = b"/home/vc/hgweb.conf"
+# enable demandloading to reduce startup time
+from mercurial import demandimport; demandimport.enable()
+from mercurial.hgweb import hgweb, wsgicgi
+app = hgweb(config)
+wsgicgi.launch(app)
